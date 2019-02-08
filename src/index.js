bd4f8137cdf2ad66417b974707b70a0e4ded85c8
Daniel Burbach <dburbach1982@gmail.com>
2019-02-08 12:54:19 -0800

    change index.js over to a Help Queue app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-function liveTime() {
-  var greeting = React.createElement('h1', {}, 'Hello, World!');
-  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-  var app = React.createElement('div', {}, greeting, clock);
+var heading = React.createElement('h1', {}, 'Help Queue');
+var ticketLocation = React.createElement('h3', {}, '3a');
+var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
+var ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
+var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
 
-  ReactDOM.render(
-    app,
-    document.getElementById('react-app-root')
-  );
-}
-
-setInterval(liveTime, 1000);
+ReactDOM.render(
+  app,
+  document.getElementById('react-app-root')
+);
